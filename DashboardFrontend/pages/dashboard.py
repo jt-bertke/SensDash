25092ddf7308472157a558b67122c06c1a5e0196
JT Bertke <jtbertke@JTs-MacBook-Pro.local>
2026-07-01 23:22:15 -0500

    working on rpm gauge

--- DashboardFrontend/pages/dashboard.py
+++ DashboardFrontend/pages/dashboard.py
@@ -1,7 +1,8 @@
 from PySide6.QtWidgets import (
     QWidget,
     QLabel,
-    QVBoxLayout
+    QVBoxLayout,
+    QFrame
 )
 
 class dashboardPage(QWidget):
@@ -9,3 +10,14 @@ class dashboardPage(QWidget):
     def __init__(self):
         super().__init__()
         
+        gauge_layout = QVBoxLayout(self)
+
+        rpm_gauge = QFrame()
+        rpm_gauge.setFrameShape(QFrame.SyledPanel)
+
+        card_layout = QVBoxLayout(rpm_gauge)
+
+        rpm_label = QLabel("RPM")
+        card_layout.addWidget(rpm_label)
+
+        gauge_layout.addWidget(rpm_gauge)
